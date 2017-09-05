1caaec99c4cd649aad64d25bee9f58f9efc569e6
luisjg <luisjguzman87@gmail.com>
2017-09-05 08:08:13 -0700

    Add the lumen proxypass package.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,9 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->configure('proxypass');
+$app->register(CSUNMetaLab\LumenProxyPass\Providers\ProxyPassServiceProvider::class);
+
 
 /*
 |--------------------------------------------------------------------------
