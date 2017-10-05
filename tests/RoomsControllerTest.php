f464bac3f9b4db1cbd4614a122125d111bbcd42a
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2017-10-05 11:13:56 -0700

    Added testGetRoom_returns_room()

--- tests/RoomsControllerTest.php
+++ tests/RoomsControllerTest.php
@@ -5,9 +5,21 @@ use App\Http\Controllers\RoomsController;
 class RoomsControllerTest extends TestCase
 {
     protected $roomsController;
+    protected $roomID;
     public function setUp(){
         parent::setUp();
         $this->roomsController = new RoomsController;
+        $this->roomID = 'JD2211';
+    }
+    public function testGetRoom_returns_room(){
+        $data = $this->roomsController->getRoom($this->roomID);
+        $this->assertEquals($data['status'],'200');
+        $this->assertEquals($data['success'],'true');
+        $this->assertEquals($data['collection'],'room');
+        $this->assertEquals($data['room']['room_number'],$this->roomID);
+        $this->assertEquals($data['room']['building_name'],'Jacaranda Hall');
+        $this->assertEquals($data['room']['latitude'],34.24141145);
+        $this->assertEquals($data['room']['longitude'],-118.529299945);
     }
     public function testGetAllRooms_returns_all_rooms(){
         $data = $this->roomsController->getAllRooms();
@@ -17,7 +29,7 @@ class RoomsControllerTest extends TestCase
         $this->assertEquals(count($data['rooms']),5944);
     }
     public function testHandleRequest_returns_room(){
-        $data = $this->call('GET', 'api/1.0/rooms?room=JD2211');
+        $data = $this->call('GET', 'api/1.0/rooms?room=' . $this->roomID);
         $content = json_decode($data->content(), true);
         $this->assertEquals($content['status'],200);
         $this->assertEquals($content['collection'],'room');
