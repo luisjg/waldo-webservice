ff1938c798fd6954fc39479539dd9f93e438dd92
luisjg <luisjguzman87@gmail.com>
2017-10-09 13:04:29 -0700

    Include a version histroy page.

---
+++ resources/views/pages/about/version-history.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Waldo Web Service</title>
+    <meta name="description" content="A Web Service that delivers information on CSUN rooms">
+    <link rel="icon" href="//www.csun.edu/sites/default/themes/csun/favicon.ico" type="image/x-icon">
+    <script src="//use.typekit.net/gfb2mjm.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
+    <link rel="stylesheet" href="{{ url('css/metaphor.css') }}">
+    <link rel="stylesheet" href="{{ url('css/tomorrow.css.min') }}">
+</head>
+<body>
+<div class="section section--sm">
+    <div class="container type--center">
+        <h1 class="giga type--thin">Waldo Web Service</h1>
+        <h3 class="h1 type--thin type--gray">Delivering CSUN Room Location Information</h3>
+    </div>
+</div>
+
+<div class="section" style="min-height: calc(100vh - 130px);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <a class="header--sm" href="{{ url('/') }}"><strong>DOCUMENTATION</strong></a>
+                <ul class="nav">
+                    <li class="nav__item"><a class="nav__link" href="{{ url('/#introduction')}}">Introduction</a></li>
+                    <li class="nav__item"><a class="nav__link" href="{{ url('/#getting-started')}}">Getting Started</a></li>
+                    <li class="nav__item"><a class="nav__link" href="{{ url('/#collections')}}">Collections</a></li>
+                    <li class="nav__item"><a class="nav__link" href="{{ url('/#subcollections')}}">Subcollections</a></li>
+                </ul>
+                <a class="header--sm" href="{{ url('/about/version-history') }}"><strong>VERSION HISTORY</strong></a>
+            </div>
+
+            <div class="col-md-9">
+                <h2 class="type--header type--thin">Version History</h2>
+                <h2>Waldo 1.0 <small>Release Date: 10/09/17</small></h2>
+                <p>
+                    <strong>New Features:</strong>
+                <ol>
+                    <li>Ability to retrieve room location information.</li>
+                </ol>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="metalab-footer">
+    <div class="metalab-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="metalab-branding">
+                        <img src="//www.csun.edu/faculty/imgs/meta-logo-horz.png" alt="CSUN META Lab Logo">
+                        <ul class="list--unstyled">
+                            <li><a href="http://metalab.csun.edu">metalab.csun.edu</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="list--unstyled metalab-tagline">
+                        <li>Explore. Learn. Go Beyond.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--
+  __  __   ___   _____     _
+ |  \/  | | __| |_   _|   /_\       Explore Learn Go Beyond
+ | |\/| | | _|    | |    / _ \      https://www.metalab.csun.edu/
+ |_|  |_| |___|   |_|   /_/ \_\
+    _       _        _     ___
+  _| |_    | |      /_\   | _ )
+ |_   _|   | |__   / _ \  | _ \
+   |_|     |____| /_/ \_\ |___/
+-->
+</body>
+</html>
