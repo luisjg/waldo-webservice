b5b2ced1de069dabc045e16a668e02e48ea45f59
luisjg <luisjguzman87@gmail.com>
2017-10-11 14:53:19 -0700

    Allow for latitude and longitude fields to be written to.

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,10 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
+    /**
+     * Table name
+     * @var string
+     */
     protected $table = 'waldo.rooms';
+
+    /**
+     * Primary key
+     * @var string
+     */
     protected $primaryKey = 'code';
+
+    /**
+     * Not incrementing
+     * @var bool
+     */
     public $incrementing = false;
 
+    /**
+     * The values that can be written/updated
+     * @var array
+     */
+    protected $fillable = [
+        'latitude',
+        'longitude'
+    ];
+
+    /**
+     * Scope query that gets a room based on ID whether it's formatted
+     * or not.
+     * @param \ $query
+     * @param string $roomId the unformatted room id
+     * @param string $formattedRoomId the formatted room id
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function scopeGetRoom($query,$roomId,$formattedRoomId){
         return $query->where('room', $roomId)
             ->orWhere('room', $formattedRoomId);
