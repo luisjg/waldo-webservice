b18b13bad444db58038925f5b8303d91cb6d33df
Matthew Fritz <matthew.fritz@csun.edu>
2018-01-05 15:22:47 -0800

    Added docblocks and formatted the code

--- app/Classes/StatePlaneMapping.php
+++ app/Classes/StatePlaneMapping.php
@@ -59,7 +59,23 @@ class StatePlaneMapping
 	 */
 	const UNITS_METERS = "meters";
 
-	public static function findLatLongFromPlateDistance($plate_lat, $plate_lon, $dx_from_plate, $dy_from_plate, $units="meters") {
+	/**
+	 * Calculates and returns the the lat/long coordinates of a point based on
+	 * the lat/long of the plate as well as the X distance (easting) and the Y
+	 * distance (northing) from the plate.
+	 * The return value is an array with a "lat" key and a "lon" key.
+	 *
+	 * @param float $plate_lat The latitude of the plate
+	 * @param float $plate_lon The longitude of the plate
+	 * @param float $dx_from_plate The distance away from the plate on the X axis
+	 * @param float $dy_from_plate The distance away from the plate on the Y axis
+	 * @param string $units Can be either "meters" or "feet" and represents the units
+	 * used for the $dx_from_plate and $dy_from_plate parameters
+	 * 
+	 * @return array
+	 */
+	public static function findLatLongFromPlateDistance($plate_lat, $plate_lon,
+		$dx_from_plate, $dy_from_plate, $units="meters") {
 		// check the units and perform conversions if necessary
 		if($units == self::UNITS_FEET) {
 			$dx_from_plate = $dx_from_plate * self::FEET_TO_METERS;
@@ -71,6 +87,21 @@ class StatePlaneMapping
 		);
 	}
 
+	/**
+	 * Calculates and returns the origin of the plate based on a known
+	 * latitude and longitude as well as an X distance (easting) and a Y
+	 * distance (northing) from that plate.
+	 * The return value is an array with a "lat" key and a "lon" key.
+	 *
+	 * @param float $known_lat The known latitude of a point
+	 * @param float $known_lon The known longitude of a point
+	 * @param float $dx_from_plate The distance to the point on the X axis from the plate
+	 * @param float $dy_from_plate The distance to the point on the Y axis from the plate
+	 * @param string $units Can be either "meters" or "feet" and represents the units
+	 * used for the $dx_from_plate and $dy_from_plate parameters
+	 * 
+	 * @return array
+	 */
 	public static function findPlateOriginFromCoordDistance($known_lat, $known_lon,
 		$dx_from_plate, $dy_from_plate, $units="meters") {
 		
@@ -92,21 +123,24 @@ class StatePlaneMapping
 
 	/**
 	 * Calculates and returns a new lat/long from an existing lat/long as well
-	 * as the X distance (easting) and Y distance (northing) from those coordinates.
+	 * as the Y distance (northing) from those coordinates.
 	 * The return value is an array with a "lat" key and a "lon" key.
 	 *
 	 * This method only works in units of meters.
 	 *
 	 * @param float $lat The existing latitude to use for the calculation
 	 * @param float $lon The existing longitude to use for the calculation
-	 * @param float $easting The X distance to add to the latitude
 	 * @param float $northing The Y distance to add to the longitude
 	 *
 	 * @return array
 	 *
 	 * @see https://stackoverflow.com/a/7478827
+	 *
+	 * The longitude formula given in that StackOverflow answer is slightly
+	 * wrong; dx should really be dy (northing) and latitude should really
+	 * be the newly-calculated value of new_longitude.
 	 */
-	private static function latLongFromDistance($lat, $lon, $easting, $northing) {
+	private static function latLongFromDistance($lat, $lon, $northing) {
 		$new_lat = $lat + ($northing / (self::EARTH_RADIUS_KILOMETERS * self::KILOMETERS_TO_METERS)) *
 			(180.0 / M_PI);
 
