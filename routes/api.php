b0db85e068eb8a57dd8c073a176f0ece6a9c404d
Matthew Fritz <matthew.fritz@csun.edu>
2018-02-09 09:42:36 -0800

    Added functionality to update the lat and long values for all rooms where those values are missing. This can be a route that is hit over cURL, for example, after a database sync

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
 $router->group(['prefix' => '1.0'], function() use ($router) {
+	$router->get('/rooms/sync', 'RoomsController@syncRoomCoordinates');
     $router->get('/rooms', 'RoomsController@handleRequest');
 });
