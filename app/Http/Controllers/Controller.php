c05b8f2f27f4d49073ee0d182771908feb246bad
luisjg <luis.guzman@metalab.csun.edu>
2018-04-05 14:56:04 -0700

    Add the sendResponse to return the json response properly.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,15 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * Returns the response as json
+     *
+     * @param $data
+     * @param int $status
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function sendResponse($data, $status = 200)
+    {
+        return response()->json($data, $status);
+    }
 }
