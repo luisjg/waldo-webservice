1aafa36a6f339e7bb6dcb9c3d7a59b3c0f5d60d4
luisjg <luis.guzman@metalab.csun.edu>
2018-04-05 15:01:39 -0700

    Re-arrange the code to accomodate the new cache workflow.

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Classes\StatePlaneMapping;
 use App\Models\Room;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Process;
 
 class RoomsController extends Controller
 {
@@ -44,59 +46,40 @@ class RoomsController extends Controller
         $allRooms = Room::all();
         $allRooms = $allRooms->map(function($roomDetail) {
             return [
-                'room_number'   => $roomDetail->room,
+                'room_number' => $roomDetail->room,
                 'building_name' => $roomDetail->building_name,
-                'latitude'      => $roomDetail->latitude,
-                'longitude'     => $roomDetail->longitude
+                'latitude' => $roomDetail->latitude,
+                'longitude' => $roomDetail->longitude
                 ];
         });
         $header = buildResponseHeaderArray(200, 'true');
-        return appendRoomDataToResponseHeader($header, 'rooms', $allRooms);
+        $formattedData = appendRoomDataToResponseHeader($header, 'rooms', $allRooms);
+        return response()->json($formattedData);
     }
 
     /**
      * Retrieves the specific rooms information
      * @param string $roomId the room ID
-     * @return array the JSON array
+     * @return \Illuminate\Http\JsonResponse
      * @internal param Request $request the request URI
      */
     public function getRoom($roomId)
     {
-        $formattedRoomId = $this->formatRoomNumber($roomId);
-        $room = Room::getRoom($roomId,$formattedRoomId)->first();
-        $response = buildResponseHeaderArray($room == null ? 404 : 200,$room == null ? 'false' : 'true');
-        if($room != null){
-            if($room->longitude != null && $room->latitude != null) {
-                $roomsCollection[] = [
-                    'room_number'   => $room->room,
-                    'building_name' => $room->building_name,
-                    'latitude'      => $room->latitude,
-                    'longitude'     => $room->longitude
-                ];
-                return appendRoomDataToResponseHeader($response, 'rooms', $roomsCollection);
-            } else {
-                // retrieve the point mapped as lat/long and save it to the
-                // database
-                $map = new StatePlaneMapping();
-                $point = $map->convertPointToLatLong
-                    ($room->x_coordinate, $room->y_coordinate);
-                $room->update([
-                    'longitude' => $point['lon'],
-                    'latitude'  => $point['lat'],
-                ]);
-                $room->touch();
-                $room->save();
-
-                $roomsCollection[] = [
-                    'room_number'   => $room->room,
-                    'building_name' => $room->building_name,
-                    'latitude'      => $room->latitude,
-                    'longitude'     => $room->longitude
-                ];
-                return appendRoomDataToResponseHeader($response, 'rooms', $roomsCollection);
-            }
+        if (File::exists(storage_path('room/'.$roomId.'.txt'))) {
+            $formattedResponse = File::get(storage_path('room/'.$roomId.'.txt'));
+            $formattedResponse = json_decode($formattedResponse);
+            $process = new Process('php ../artisan update:room ' . $roomId . ' > /dev/null &');
+            $process->start();
+            return $this->sendResponse($formattedResponse);
         } else {
-            return appendErrorDataToResponseHeader($response);
+            $formattedResponse = formatRoomCollection($roomId);
+            if ($formattedResponse) {
+                return $formattedResponse;
+            } else {
+                $response = buildResponseHeaderArray(404, 'false');
+                $formattedResponse = appendErrorDataToResponseHeader($response);
+                return $this->sendResponse($formattedResponse, 404);
+            }
         }
     }
 
@@ -110,14 +93,14 @@ class RoomsController extends Controller
         $rooms = Room::whereNull('latitude')
             ->whereNull('longitude')
             ->get();
-        if($rooms->count() > 0) {
+        if ($rooms->count() > 0) {
             $map = new StatePlaneMapping();
-            foreach($rooms as $room) {
+            foreach ($rooms as $room) {
                 $point = $map->convertPointToLatLong
                     ($room->x_coordinate, $room->y_coordinate);
                 $room->update([
                     'longitude' => $point['lon'],
-                    'latitude'  => $point['lat'],
+                    'latitude' => $point['lat'],
                 ]);
                 $room->touch();
                 $room->save();
@@ -131,71 +114,5 @@ class RoomsController extends Controller
 
         $response = buildResponseHeaderArray(200, "true");
         return appendMessageDataToResponseHeader($response, $message);
-    }
-
-    /**
-     * Prepends 1 or more zeroes to a number.
-     * @param string|int $num - The number to which you want to prepend zeroes
-     * @param int $count - (optional) How many zeroes you would like to prepend. Defaults to 1.
-     * @return int|string
-     */
-    private function prependZeroes($num, $count=1) {
-        for ($i = 0; $i < $count; $i++) {
-            $num = '0' . $num;
-        }
-        return $num;
-    }
-
-    /**
-     * For some rooms like SH 173, they are stored in the database with 4-digit numbers by prepending zeros.
-     * However, some rooms  like UV00D8 are stared as 2-digits. Keep that in mind when using this method to help query
-     * the database. This also removes whitespace in the name.
-     * @param $room - Un-formatted Room with  less than 4 digits (ex:  "SH 175").
-     * @return mixed|string  - The formatted room string with 4 digits and no spaces (ex: "SH0175") ready for querying.
-     */
-    private function formatRoomNumber($room) {
-        // Remove whitespace and case.
-        $room = preg_replace('/\s/','',strtoupper($room));
-
-        // The number of digits a room has, according to the database.
-        $desiredNumberOfDigits = 4;
-
-        // This is the regex to which all room numbers should conform before hitting the database.
-        // The x flag allows comments in regex.
-        $desiredFormat = "/
-                            [A-Z]+                               # This matches the 2-letter Building code.
-                            [0-9]{1,$desiredNumberOfDigits}       # This matches the number part.
-                            (                                    # This group matches the optional alphanumeric suffix.
-                                [A-Z]                            # It must start with a letter to be distinguishable.
-                                [A-Z0-9]*                        # May contain a mix of numbers and letters.
-                            )?                                   # Let it be optional.
-                         /x";
-
-        // A callback to a regex that matches a string of digits.
-        $insertNeededZeroesCallback = function($matches) use ($desiredNumberOfDigits) {
-            //Match holds the 2210 in JD2210
-            $match = $matches[0];
-            $numberOfNeededZeroes = $desiredNumberOfDigits - strlen($match);
-            return $this->prependZeroes($match, $numberOfNeededZeroes);
-        };
-
-        // A callback to a regex that matches a valid room format.
-        $ensureProperFormatCallback = function($matches) use($desiredNumberOfDigits, $insertNeededZeroesCallback) {
-            $match = $matches[0];
-            $replacement = preg_replace_callback(
-                "/[0-9]{1,$desiredNumberOfDigits}/",
-                $insertNeededZeroesCallback,
-                $match
-            );
-            return $replacement;
-        };
-
-        // Perform the actual formatting of the string.
-        $room = preg_replace_callback(
-            $desiredFormat,
-            $ensureProperFormatCallback,
-            $room
-        );
-        return $room;
     }
 }
